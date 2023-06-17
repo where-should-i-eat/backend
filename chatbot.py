675ac1356e0caddbd1229f54683dad653494c16c
Ryan Campbell <ryancampbell@berkeley.edu>
2023-06-17 14:44:33 -0700

    chatbot

--- chatbot.py
+++ chatbot.py
@@ -1,29 +1,50 @@
 import openai
+import re
 
 with open('API_KEY.txt', 'r') as file:
     API_KEY = file.read().rstrip()
 
 openai.api_key = API_KEY
 
-def init_chatbot(model="gpt-3.5-turbo"):
-    response = input("What do you want to buy?\n")
+def shopping_assistant(model="gpt-3.5-turbo", max_tokens=100):
+    done = False
+    response = input("What do you want to eat?\n")
     messages_history = [
-        {"role": "system", "content": "You are a helpful shopping assistant. Your goal is to get tags to search Craigslist with for relevant items. When you are done with your task, say DONE."},
+        {"role": "system", "content": "You are a helpful restaurant assistant. Your goal is to determine types of food that people want. When you are done with your task, say DONE."},
         {"role": "assistant", "content": "What do you want to buy?"}]
-    chatbot(response, messages_history, model)
+    while not done:
+        messages_history += [{"role": "user", "content": response}]
 
-def chatbot(response, messages_history, model):
-    messages_history += [{"role": "user", "content": response}]
+        output = openai.ChatCompletion.create(
+            model=model,
+            messages=messages_history,
+            max_tokens=max_tokens
+            )
+        output_text = output['choices'][0]['message']['content']
+        messages_history += [{"role": "assistant", "content": output_text}]
 
+        response = input(output_text + "\n")
+
+        done = True # somehow figure out when the tags have been given.
+
+    tags = extract_tags(messages_history, model=model, max_tokens=max_tokens)
+
+    return tags
+
+def extract_tags(messages_history, model="gpt-3.5-turbo", max_tokens=100):
+    messages_history = messages_history[1:] + [{"role": "system", "content": "Now you are a tag extraction system. Extract the given tags into the format of a python list of strings."}]
     output = openai.ChatCompletion.create(
-        model=model,
-        messages=messages_history,
-        max_tokens=100
-    )
+            model=model,
+            messages=messages_history,
+            max_tokens=max_tokens
+            )
     output_text = output['choices'][0]['message']['content']
-    messages_history += [{"role": "assistant", "content": output_text}]
 
-    next_response = input(output_text + "\n")
-    chatbot(next_response, messages_history, model)
+    print(output_text)
 
-init_chatbot()
+    pattern = r"\[(.*?)\]"
+    tags = re.findall(pattern, output_text)
+    return tags
+
+tags = shopping_assistant()
+print(tags)

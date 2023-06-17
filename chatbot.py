d6336e2d0f3785ad06ad70df3d90422485700f9f
Ryan Campbell <ryancampbell@berkeley.edu>
2023-06-17 12:27:42 -0700

    chatbot update

--- chatbot.py
+++ chatbot.py
@@ -8,7 +8,7 @@ openai.api_key = API_KEY
 def init_chatbot(model="gpt-3.5-turbo"):
     response = input("What do you want to buy?\n")
     messages_history = [
-        {"role": "system", "content": "You are a helpful shopping assistant. Only answer queries relating to shopping. Your goal is to get tags to search Craigslist with for relevant items."},
+        {"role": "system", "content": "You are a helpful shopping assistant. Your goal is to get tags to search Craigslist with for relevant items. When you are done with your task, say DONE."},
         {"role": "assistant", "content": "What do you want to buy?"}]
     chatbot(response, messages_history, model)
 
@@ -21,8 +21,9 @@ def chatbot(response, messages_history, model):
         max_tokens=100
     )
     output_text = output['choices'][0]['message']['content']
+    messages_history += [{"role": "assistant", "content": output_text}]
+
     next_response = input(output_text + "\n")
-    
     chatbot(next_response, messages_history, model)
 
 init_chatbot()

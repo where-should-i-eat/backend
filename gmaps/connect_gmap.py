4b23860c9f44f9d8368794f10fff747e266f3ccf
Harryllh <107988441+Harryllh@users.noreply.github.com>
2023-06-17 23:30:40 -0700

    location function added

--- gmaps/connect_gmap.py
+++ gmaps/connect_gmap.py
@@ -11,10 +11,24 @@ GMAPS_API_KEY = os.environ.get("GMAPS_API_KEY")
 
 gmaps = googlemaps.Client(key=GMAPS_API_KEY)
 
+# print(gmaps.reverse_geocode((37.8719, -122.2585))[0]['address_components'])
+# print(gmaps.reverse_geocode((37.8719, -122.2585))[0]['formatted_address'])
+geocode_result = gmaps.geocode(address="2580 Bancroft Way")
+# print(geocode_result[0]['geometry']['location'])
+# print(gmaps.distance_matrix((37.8719, -122.2585), (39, -122.2585), units='metric')['rows'][0]['elements'][0]['distance']['value'])
+# exit()
 
-def get_restaurant_recommendations(query, longitude, latitude):
+# def get_top5_review(r):
+#     sorted_reviews = sorted(r, key=lambda x: x['rating'], reverse=True)
+#     # Select the top 5 reviews
+#     if len(sorted_reviews < 5):
+#         return sorted_reviews
+#     else:
+#         return  sorted_reviews[:5]
+
+def get_restaurant_recommendations(query, lat_lng):
     try:
-        restaurants = gmaps.places(query=query, location = (longitude, latitude), type='restaurant', min_price=1, max_price=4, open_now=True)
+        restaurants = gmaps.places(query=query, location=lat_lng, type='restaurant', min_price=1, open_now=True)
         
         recommendations = []
         
@@ -26,12 +40,20 @@ def get_restaurant_recommendations(query, longitude, latitude):
             phone_number = restaurant.get('formatted_phone_number', 'N/A')
             place_id = restaurant['place_id']
             reviews = get_restaurant_reviews(place_id)
+
             photos = get_restaurant_photos(place_id) # a list of url's
+            coordinate = gmaps.geocode(address=address)[0]['geometry']['location']
+            distancedic = gmaps.distance_matrix(coordinate, lat_lng, units='metric')
+            print(distancedic['rows'][0]['elements'][0])
+            distance = 'Unknown'
+            if distancedic['rows'][0]['elements'][0]['status'] == 'OK':
+                distance = distancedic['rows'][0]['elements'][0]['distance']['text'] #the distance is in km
             
             if rating >= min_rating:
                 recommendations.append({'name': name, 'address': address, 'rating': rating, 
                                         'website': website, 'phone number': phone_number, 
-                                        'place_id': place_id, 'reviews': reviews, 'photos': photos})
+                                        'place_id': place_id, 'reviews': reviews[:3], 'photos': photos,
+                                        'coordinate': coordinate, 'distance': distance})
         
         return recommendations
     
@@ -45,19 +67,19 @@ min_rating = 4.0
 query = "Give me some Italian food"
 
 
-def get_location():
-    url = f"https://www.googleapis.com/geolocation/v1/geolocate?key={GMAPS_API_KEY}"
-    response = requests.post(url)
+# def get_location():
+#     url = f"https://www.googleapis.com/geolocation/v1/geolocate?key={GMAPS_API_KEY}"
+#     response = requests.post(url)
 
-    if response.status_code == 200:
-        result = response.json()
-        location = result["location"]
-        latitude = location["lat"]
-        longitude = location["lng"]
-        accuracy = result["accuracy"]
-    else:
-        print("Failed to get location")
-    return result
+#     if response.status_code == 200:
+#         result = response.json()
+#         location = result["location"]
+#         latitude = location["lat"]
+#         longitude = location["lng"]
+#         accuracy = result["accuracy"]
+#     else:
+#         print("Failed to get location")
+#     return result
 
 def get_restaurant_reviews(place_id):
     url = f"https://maps.googleapis.com/maps/api/place/details/json?place_id={place_id}&fields=name,rating,reviews&key={GMAPS_API_KEY}"
@@ -88,7 +110,6 @@ def get_restaurant_photos(place_id):
         if 'photos' in result:
                 photo_references = [photo['photo_reference'] for photo in result['photos']]
 
-                
                 # Use the photo references to retrieve the actual photos
                 for reference in photo_references:
                     photo_url = f"https://maps.googleapis.com/maps/api/place/photo?key={GMAPS_API_KEY}&photoreference={reference}&maxheight=500"
@@ -100,8 +121,11 @@ def get_restaurant_photos(place_id):
 
     return photo_urls
 
+def add_to_coordinate(add):
+    geocode_result = gmaps.geocode(address=add)
+    return geocode_result[0]['geometry']['location'] # a dictionary of lat and lng
 
-# def 
 
-
-# print(get_restaurant_recommendations(query, 112, 33))
+lat_lng = (37.8719, -122.2585)
+query = "find me some chinese place near campus"
+get_restaurant_recommendations(query, lat_lng)
--- gmaps/top5.py
+++ gmaps/top5.py
@@ -3,31 +3,49 @@ import dotenv
 import os
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import re
 # from chat.chatbot import chatbot
-from connect_gmap import get_restaurant_recommendations
+from connect_gmap import get_restaurant_recommendations, add_to_coordinate
 from typing import List, Dict
 
 dotenv.load_dotenv()
 openai.api_key = os.environ.get("API_KEY")
 
 
-def get_top5(messages_history, recommendations, model="gpt-3.5-turbo", max_tokens=100):
-    # TODO: figure out distance, reviews
-    modified_recs = [{'name': r['name'], 'rating': r['rating'], 'location': r['address']} for r in recommendations]
-    message = f"Help me now to determine the top 5 choices from this list of choices: {modified_recs}."
+def extract_coordinate(text):
+    address_pattern = r"(\d+\s+[\w\s]+,\s+[\w\s]+,\s+\w+)"
+    addresses = []
+    matches = re.findall(address_pattern, text)
+    for match in matches:
+        addresses.append(match)
+    return [add_to_coordinate(a) for a in addresses]
 
-    messages_history += [{"role": "user", "content": message}]
+def get_top5(messages_history, recommendations, model="gpt-3.5-turbo", max_tokens=100):
+    # TODO reviews
+    modified_recs = [r['name'] + "/" + f"rating: {r['rating']}" + "/" + f"distance: {r['distance']}" for r in recommendations]
+    findmsg = f"Give me top 3 choices from this list of choices: {modified_recs}. Give me the name of the restaurant and the address"
+    name_address = [r['name'] + ":" + r['address'] for r in recommendations]
+    addressmsg = f"Tell me the exact address of the restaurant you just chose from this list {name_address}"
+
+    messages_history += [{"role": "user", "content": findmsg}]
 
     output = openai.ChatCompletion.create(
         model=model,
         messages=messages_history,
         max_tokens=max_tokens
         )
-    output_text = output['choices'][0]['message']['content']
-    messages_history += [{"role": "assistant", "content": output_text}]
-    messages_history.pop(-2)
+    output_text1 = output['choices'][0]['message']['content']
+    messages_history += [{"role": "assistant", "content": output_text1}]
+    messages_history += [{"role": "user", "content": addressmsg}]
 
-    return output_text
+    output_text2 = output['choices'][0]['message']['content']
+    messages_history += [{"role": "assistant", "content": output_text2}]
+    print("coordinates:", extract_coordinate(output_text2))
+    messages_history.pop(-2)
+    messages_history.pop(-3)
+    messages_history.pop(-4)
+
+    return output_text2
 
 
 def chatbot(messages_history: List[Dict[str, str]], model="gpt-3.5-turbo", max_tokens=400):
@@ -49,16 +67,15 @@ def chatbot(messages_history: List[Dict[str, str]], model="gpt-3.5-turbo", max_t
     messages_history += [{"role": "assistant", "content": output_text}]
     messages_history.pop(0)
 
-    # print(messages_history)
-
-
     return messages_history
 
 mes_hist = chatbot([])
 mes_hist.append({"role": "user", "content": "we want to get meat close to Berkeley campus"})
 mes_hist = chatbot(mes_hist)
 
-reco = get_restaurant_recommendations("meat food", -122.257740, 37.868710)
+reco = get_restaurant_recommendations("meat food", (-122.257740, 37.868710))
 
 output_text = get_top5(mes_hist, reco)
 print(output_text)
+
+
